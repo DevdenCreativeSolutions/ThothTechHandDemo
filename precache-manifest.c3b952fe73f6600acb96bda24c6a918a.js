a8a6df00f319fb4521976f373baeb2dd64b8affd
ashishdevden <ashish100rana123@gmail.com>
2020-10-19 19:26:10 +0530

    Updates

---
+++ precache-manifest.c3b952fe73f6600acb96bda24c6a918a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ae4e8694801d157269452f7b2d2aac",
+    "url": "/DevdenCreativeSolutions/ThothTechHandDemo/index.html"
+  },
+  {
+    "revision": "50b99be2331c3957177e",
+    "url": "/DevdenCreativeSolutions/ThothTechHandDemo/static/css/main.9b1ab70d.chunk.css"
+  },
+  {
+    "revision": "64cd449ca435b6a203d6",
+    "url": "/DevdenCreativeSolutions/ThothTechHandDemo/static/js/2.e3477e34.chunk.js"
+  },
+  {
+    "revision": "a5db693410fed6a67f010a07b976fd8f",
+    "url": "/DevdenCreativeSolutions/ThothTechHandDemo/static/js/2.e3477e34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50b99be2331c3957177e",
+    "url": "/DevdenCreativeSolutions/ThothTechHandDemo/static/js/main.b305b8ff.chunk.js"
+  },
+  {
+    "revision": "1ebda60d8548e6039d93",
+    "url": "/DevdenCreativeSolutions/ThothTechHandDemo/static/js/runtime-main.e41a1d77.js"
+  }
+]);
